eb7b40c96a0deabfa34bfae951c12ce002d33881
abhay <ap3919529@gmail.com>
2024-02-02 14:38:12 +0530

    feat:notifications

--- index.js
+++ index.js
@@ -1,18 +1,37 @@
 const btn = document.getElementById("btn");
+const convinceBtn = document.getElementById("convince");
 
 document.addEventListener("DOMContentLoaded", () => {
   if (!("Notification" in window)) {
     alert("Browser does not support notifications");
   } else {
     function handleNotification() {
-      Notification.requestPermission().then((result) => console.log(result));
+      Notification.requestPermission().then((result) => {
+        if (result === "granted") {
+          alert("Maan gaye");
+          btn.style.display = "none";
+        } else {
+          alert("Main nahi maan rahe/raha");
+        }
+      });
     }
 
-    if (Notification.permission === "granted") {
+    function handleConvince() {
+      const notification = new Notification("Khana khaya kya", {
+        body: "Aap nahi kahogi toh main bhi nahi khaunga",
+      });
+
+      if (Notification.permission != "granted") {
+        alert("Abe pahele permission le");
+      }
     }
   }
 
   btn.addEventListener("click", () => {
     handleNotification();
+  });
+
+  convinceBtn.addEventListener("click", () => {
+    handleConvince();
   });
 });
